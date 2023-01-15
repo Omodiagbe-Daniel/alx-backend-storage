96c2dd5ea2de57b2eb57d06986174ce259d16009
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-15 22:50:38 +0100

    made correction

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a trigger that resets the attribute
 -- valid_email only when the email has been changed
-CREATE TRIGGER change_email AFTER INSERT ON users
+CREATE TRIGGER change_email BEFORE INSERT ON users
     FOR EACH ROW
     BEGIN
         IF valid_email == 0 THEN
