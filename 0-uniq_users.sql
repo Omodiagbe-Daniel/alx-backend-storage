2682e1cdda9ceda4406632eec4b017de8666e21c
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-15 21:00:03 +0100

    SQL script that lists all bands with Glam rock as their main style, ranked by their longevity

--- 0-uniq_users.sql
+++
@@ -1,6 +0,0 @@
--- create a new table if it does not exist
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
