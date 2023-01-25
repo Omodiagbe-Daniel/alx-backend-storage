41d3d047b01b69a34e4f0fe94991e4d3cb078b92
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-25 14:16:25 +0100

    a Python script that provides some stats about Nginx logs stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats
+   about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.logs
+    collection = db.nginx
+    no_doc = (collection.count_documents({}))
+    print(f"{no_doc} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for meth in methods:
+        res = collection.count_documents({"method": meth})
+        print(f"\tmethod {meth}: {res}")
+    rest = collection.count_documents({"path": "/status"})
+    print(f"{rest} status check")
