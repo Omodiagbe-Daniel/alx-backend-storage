4b65ec7afe7dd853173d9d055cb32ba0fe712b08
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-25 12:30:56 +0100

    made update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,10 +9,8 @@ def update_topics(mongo_collection, name, topics):
         Python function that changes all topics of
         a school document based on the name
     """
-    mongo_collection.update_many(
-        {"name": name},
-        {"$set":
-            {"topic" : topics}
-        },
-        upsert=False
-    )
+    res = mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topic": topics}}
+        )
+    return res
